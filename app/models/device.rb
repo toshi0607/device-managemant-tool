ec1a029a9f0c50c95d32e9143d81823d5c05a872
杉田　寿憲 <s.toshi0607@gmail.com>
2014-12-17 10:06:52 +0900

    資産管理IDが一意である制約

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,6 @@
 class Device < ActiveRecord::Base
 	validates :name            , presence: true
-	validates :property_id     , presence: true, length: { is: 13 }
+	validates :property_id     , presence: true, length: { is: 13 }, uniqueness: true
 	validates :devinfo_id      , presence: true
 	validates :acquisition_date, presence: true
 end
