9a79d436572c3af3cba92b6a4715d45faf01db18
杉田　寿憲 <s.toshi0607@gmail.com>
2014-12-17 09:52:59 +0900

    必須項目の設定

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,2 +1,6 @@
 class Device < ActiveRecord::Base
+	validates :name            , presence: true
+	validates :property_id     , presence: true
+	validates :devinfo_id      , presence: true
+	validates :acquisition_date, presence: true
 end
