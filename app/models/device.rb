c819648f9836c6816c8b31602b48a1f41624833f
杉田　寿憲 <s.toshi0607@gmail.com>
2014-12-17 10:04:20 +0900

    資産管理IDの文字数設定

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,6 @@
 class Device < ActiveRecord::Base
 	validates :name            , presence: true
-	validates :property_id     , presence: true
+	validates :property_id     , presence: true, length: { is: 13 }
 	validates :devinfo_id      , presence: true
 	validates :acquisition_date, presence: true
 end
