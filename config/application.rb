ccac88406b746038e5e5fcb898e224ec9d5b48a5
杉田　寿憲 <s.toshi0607@gmail.com>
2014-12-22 11:16:52 +0900

    RSpec設定

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,15 @@ module DeviceManagementTool
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
     config.encoding = "utf-8"
-  end
+
+    config.generators do |g|
+        g.test_framework :rspec,
+            fixtures: true,
+            view_specs: false,
+            helper_specs: false,
+            routing_specs: false,
+            controller_specs: true,
+            request_specs: false
+        g.fixture_replacement :factory_girl, dir: "spec/factories" end
+    end
 end
